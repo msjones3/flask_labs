8e9e2e2b37e738c8b6b3fda3adf03a2dd317ae7a
msjones3 <51344555+msjones3@users.noreply.github.com>
2020-11-16 16:13:07 +1000

    fix error

--- formValidation.py
+++ formValidation.py
@@ -13,11 +13,12 @@ def index():
 @app.route('/form_submission', methods=['GET', 'POST'])
 def submit():
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or \
-                request.form['password'] != 'secret':
+        # username must be 'admin' and password must be 'secret'
+        if request.form['username'] != 'admin' or request.form['password'] != 'secret':
                 flash('invalid credentials')
-                redirect(url_for('index'))
+                return redirect(url_for('index'))
         else:
+            # all validation testing passed. Log the user in. 
             flash('You were successfully logged in')
             return redirect(url_for('index'))
 
